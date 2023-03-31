ee6f606e77be3b962e3ec0200187e2d3eb9e486e
Maksym Kravchenko <kmaksym@gmail.com>
2023-03-31 14:29:01 -0700

    goit-js-hw-07
    complete 2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,90 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEL = document.querySelector('.gallery');
+const markup = galleryItems.map(({ description, preview, original}) =>
+    `<li class='gallery__item'>
+    <a class="gallery__link" href="${original}">
+    <img 
+    class= "gallery__image" 
+    src="${preview}" 
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>`).join('')
+galleryEL.insertAdjacentHTML('beforeend', markup)
+
+galleryEL.addEventListener('click', onClick)
+function onClick(eve) {
+    eve.preventDefault()
+    const instance = basicLightbox.create(`<div><img src=${eve.target.dataset.source}></div>`
+    )
+    instance.show();
+    const originalEL=document.querySelector('.basicLightbox')
+    document.addEventListener('keydown', onEscape)
+    function onEscape(eve) {
+    if(eve.keyCode === 27) {originalEL.remove()};
+        }; 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let idNum = Number(0);
+// const id = galleryItems.map(item => {
+//     idNum++
+//     item.id = idNum
+// }
+// )
+
+// // const markup = galleryItems.map(({ description, preview, id }) =>
+// //     `<li data-img-id='${id}' class='js-card'>
+// //     <img class= "gallery__image" src="${preview}" alt="${description}">
+// //     </li>`).join('')
+
+// galleryEL.insertAdjacentHTML('beforeend', markup)
+
+// galleryEL.addEventListener('click', onClick)
+// function onClick(eve) {
+//     const imgId = eve.target.closest('.js-card').dataset.imgId
+//     const currentItem = galleryItems.find(({ id }) => id === Number(imgId));
+//     const instance = basicLightbox.create(`
+//  <div><img src=${currentItem.original} alt=${currentItem.description}></div>
+//  `
+//     )
+//     instance.show();
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEL = document.querySelector('.gallery');
+const markup = galleryItems.map(({ description, preview, original}) =>
+    `<li class='gallery__item'>
+    <a class="gallery__link" href="${original}">
+    <img 
+    class= "gallery__image" 
+    src="${preview}" 
+    alt="${description}"
+    />
+    </a>
+    </li>`).join('')
+galleryEL.insertAdjacentHTML('beforeend', markup)
+
+const gallery = new SimpleLightbox('.gallery__item a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+gallery.next();
